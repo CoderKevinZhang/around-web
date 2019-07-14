7d29d6edd510f7200bc0a3315251fd688f796e4b
CoderKevinZhang <iaminwc@163.com>
2019-07-14 15:27:28 -0700

    update

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,15 +4,15 @@ import logo from '../assets/images/logo.svg';
 
 export class TopBar extends React.Component {
  render() {
-   return (
-     <header className="App-header">
-       <img src={logo} className="App-logo" alt="logo" />
-       <div className="App-title">Around</div>
-       {this.props.isLoggedIn ?
-         <a className="logout" onClick={this.props.handleLogout} >
-           <Icon type="logout"/>{' '}Logout
-         </a> : null}
-     </header>
-   );
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-title">Around</div>
+      {this.props.isLoggedIn ?
+        <a className="logout" onClick={this.props.handleLogout} >
+          <Icon type="logout"/>{' '}Logout
+        </a> : null}
+    </header>
+  );
  }
 }
