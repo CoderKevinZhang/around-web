f396b1487b403fb988858edc13c4997a26f31409
CoderKevinZhang <iaminwc@163.com>
2019-07-21 20:55:40 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+export const API_ROOT = 'http://35.208.95.166:8080';
 export const TOKEN_KEY = 'TOKEN_KEY';
 export const GEO_OPTIONS = {
   enableHighAccuracy: true,
